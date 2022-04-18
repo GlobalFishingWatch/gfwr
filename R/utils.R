e884cb97df18e2cefc30189803ce667cb6076057
Tyler <tyler.clavelle@gmail.com>
2022-04-18 16:49:31 -0400

    added generic pagination function and included in get_events

--- R/utils.R
+++ R/utils.R
@@ -34,3 +34,38 @@ make_char <- function(col) {
 make_datetime <- function(x) {
   as.POSIXct(as.character(x), format = '%Y-%m-%dT%H:%M:%S', tz = 'UTC')
 }
+
+#' Pagination function for GFW API calls
+#' @name paginate
+#' @keywords internal
+#' @export
+#' @return
+# pagination function
+paginate <- function(response, endpoint, key){
+
+  # List to store responses
+  responses <- list()
+  responses[[1]] <- response
+  # Current page values
+  total <- response$total
+  next_off <- response$nextOffset
+
+  # While nextOffset is less than total, pull additional response pages
+  while(next_off < total){
+
+    # Event datasets to pass to param list
+    next_endpoint <- httr::modify_url(endpoint, query = list(offset = next_off))
+    # API call for next page
+    next_response <- httr::GET(next_endpoint,
+                               config = httr::add_headers(Authorization = paste("Bearer", key, sep = " "))) %>%
+      httr::content()
+
+    # Append response to list
+    responses[[length(responses)+1]] <- next_response
+
+    # Pull out nextOffset of latest API response
+    next_off <- next_response$nextOffset
+  }
+  # Return list of response pages
+  return(responses)
+}
