3f6a9b4dbdc525e816405d35c010d5fe24acea78
Nate Miller <nate@globalfishingwatch.org>
2022-04-29 13:55:03 -0500

    format get_raster with v2 parameter names

--- R/get_raster.R
+++ R/get_raster.R
@@ -1,11 +1,12 @@
-#'Base function to get event from API and convert response to data frame
+#'Base function to get raster from API and convert response to data frame
 #'
-#' @param event_type Type of event to get data of. It can be "port_visit" or "fishing"
-#' @param response_limit Number of events to import. We need some documentation for the max.
-#' @param vessel VesselID. How to get this?
-#' @param include_regions Whether to include regions? Ask engineering if this can always be false
-#' @param start_date Start of date range to search events
-#' @param end_date End of date range to search events
+#' @param spatial_resolution raster spatial resolution. Can be "low" = 0.1 degree or "high" = 0.01 degree
+#' @param temporal_resolution raster temporal resolution. Can be 'daily','monthly','yearly'.
+#' @param group_by parameter to group by. Can be 'vessel_id', 'flag', 'geartype', 'flagAndGearType'
+#' @param start_date Start of date range for raster
+#' @param end_date End of date range for raster
+#' @param format output format. Current support for 'csv'.
+#' @param shape_json geojson, shape to filter raster
 #' @param key Authorization token. Can be obtained with gfw_auth function
 #'
 #' @importFrom dplyr across
@@ -21,21 +22,21 @@
 #' @importFrom tidyr unnest_wider
 #' @importFrom tidyselect everything
 
-get_raster <- function(spatialResolution = NULL,
-                       temporalResolution = NULL,
-                       groupBy = NULL,
+get_raster <- function(spatial_resolution = NULL,
+                       temporal_resolution = NULL,
+                       group_by = NULL,
                        start_date = NULL,
                        end_date = NULL,
-                       format= NULL,
+                       format= 'csv',
                        shape_json = NULL,
                        key
 ) {
 
   # Event datasets to pass to param list
   endpoint <- get_endpoint(dataset_type = 'raster',
-                           spatialResolution = spatialResolution,
-                           temporalResolution = temporalResolution,
-                           groupBy = groupBy,
+                           `spatial-resolution` = spatial_resolution,
+                           `temporal-resolution` = temporal_resolution,
+                           `group-by` = group_by,
                            start_date = start_date,
                            end_date = end_date,
                            format = format)
