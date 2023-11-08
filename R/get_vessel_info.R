94f0d11248fcbc1d14ab75a94230f448693b3dad
Andrea Sánchez-Tapia <4183062+AndreaSanchezTapia@users.noreply.github.com>
2023-11-08 12:51:07 -0800

    parses response to a list of tibbles. working so far for all examples.

--- R/get_vessel_info.R
+++ R/get_vessel_info.R
@@ -2,10 +2,6 @@
 #'
 #' @param search_type Type of vessel search to perform. Can be `"search"` or
 #' `"id"`. (Note:`"advanced"` and `"basic"` are no longer in use as of gfwr 2.0.0.)
-#' @param query Basic search terms to identify vessel. Has to be a length-1
-#' numeric or character vector with MMSI, IMO, CALL SIGN or Ship name
-#' @param where Optional. SQL syntax to perform advanced search. Incompatible
-#' with `query`
 #' @param match_fields Optional. Allows to filter by `matchFields` levels.
 #' Possible values: SEVERAL_FIELDS, NO_MATCH, ALL. Incompatible with `where`
 #' @param includes Enhances the response with new information.
@@ -20,6 +16,7 @@
 #' objects, only the delta or the latest. Possible values: `"NONE"`, `"DELTA"`,
 #' `"ALL"`
 #' @param key Authorization token. Can be obtained with `gfw_auth()` function
+#' @param ... Other parameters
 
 #' @importFrom httr2 req_headers
 #' @importFrom httr2 req_perform
@@ -38,25 +35,30 @@
 #' @examples
 #' library(gfwr)
 #' # Simple searches, using includes
-#' get_vessel_info(query = c(224224000, 224224001), search_type = "search", dataset =
-#'  "fishing_vessel", key = gfw_auth())
-#' get_vessel_info(query = 224224000, search_type = "search", dataset =
-#'  "fishing_vessel", includes = "OWNERSHIP", key = gfw_auth())
-#' get_vessel_info(query = 224224000, search_type = "search", dataset =
-#'  "fishing_vessel", includes = c("OWNERSHIP", "AUTHORIZATIONS"), key = gfw_auth())
+#' get_vessel_info(query = 224224000, search_type = "search",
+#' key = gfw_auth())
+#' get_vessel_info(query = 224224000, search_type = "search",
+#'includes = "OWNERSHIP", key = gfw_auth())
+#' get_vessel_info(query = 224224000, search_type = "search",
+#'includes = c("OWNERSHIP", "AUTHORIZATIONS"), key = gfw_auth())
 #' # Advanced search with where instead of query:
 #' get_vessel_info(where = "ssvid = '441618000' OR imo = '9047271'",
-#'  search_type = "search", dataset = "fishing_vessel",
-#'   includes = c("OWNERSHIP", "AUTHORIZATIONS"), key = gfw_auth())
+#' search_type = "search",
+#' includes = c("OWNERSHIP", "AUTHORIZATIONS"),
+#' key = gfw_auth())
 #'  # Vessel id search
 #'  get_vessel_info(search_type = "id",
 #'  ids = c("8c7304226-6c71-edbe-0b63-c246734b3c01",
 #'  "6583c51e3-3626-5638-866a-f47c3bc7ef7c"), key = gfw_auth())
 #'  get_vessel_info(search_type = "id",
-#'  ids = c("8c7304226-6c71-edbe-0b63-c246734b3c01"), registries_info_data = c("ALL"), key = gfw_auth())
+#'  ids = c("8c7304226-6c71-edbe-0b63-c246734b3c01"),
+#'  registries_info_data = c("ALL"), key = gfw_auth())
 #' @export
 get_vessel_info <- function(search_type = "search",
+                            #query = NULL,
+                            #where = NULL,
                             includes = NULL,
+                            match_fields = NULL,
                             ids = NULL,
                             registries_info_data = NULL,
                             key = gfw_auth(),
@@ -68,13 +70,8 @@ get_vessel_info <- function(search_type = "search",
     search_type <- "search"
   }
 
-  #if (!missing(query) & !is.null(where)) warning("where is incompatible with query. for advanced search, use only where")
-
   #endpoint <<- get_identity_endpoint(
-   # dataset_type = dataset,
     #search_type = search_type,
-    #query = query,
-    #where = where,
     #includes = includes,
     #limit = 99999,
     #offset = 0
@@ -124,11 +121,16 @@ endpoint <- base %>%
     #httr2::req_error(., body = gist_error_body) %>%
     httr2::req_user_agent(gfw_user_agent()) %>%
     httr2::req_perform() %>%
-    httr2::resp_body_json()
+    httr2::resp_body_json(simplifyVector = TRUE)
 
-  output <- response
-    tibble::enframe(response$entries) %>%
-    tidyr::unnest_wider(data = ., col = value)
+  output <- list(
+    dataset = purrr::pluck(response$entries, "dataset"),
+    registryInfoTotalRecords = purrr::pluck(response$entries, "registryInfoTotalRecords"),
+    registryInfo = purrr::map(response$entries$registryInfo, tibble::tibble),
+    registryOwners = purrr::map(response$entries$registryOwners, tibble::tibble),
+    registryPublicAuthorizations = purrr::map(response$entries$registryPublicAuthorizations, tibble::tibble),
+    selfReportedInfo = purrr::map(response$entries$registryselfReportedInfo, tibble::tibble)
+    )
 
   return(output)
 }
