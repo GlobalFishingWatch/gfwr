6bc55c6c383784e599ca2b26ca146233b3708674
Jorge Cornejo <cornejotux@gmail.com>
2026-02-05 15:46:25 -0300

    Some minor correctios and anotatios to the file.

--- R/test_gfw_bulk_report.R
+++ R/test_gfw_bulk_report.R
@@ -2,20 +2,20 @@
 
 ## This creates a local file with data!
 res <- gfw_bulk_report(
-  name = "fixed-infra-argentina-oil-2020-2025",
+  name = "fixed-infra-Chile-2020-2025", ## User define name por the report!
   dataset = "public-fixed-infrastructure-data:v1.1",
   format = "CSV",
-  region_dataset = "public-eez-areas",
+  region_dataset = "public-eez-areas", ## We should try to test with a User Defines AOI
   region_id = 8465, #Chile -- #8466 #Argentina
   filters = c(
-    "label = 'oil'",
-    "structure_start_date between '2020-01-01' and '2025-01-01'"
+    "label = 'oil'",  #Oprtion are (oil, wind, unknown)
+    "structure_start_date between '2020-01-01' and '2027-01-01'"
   ),
-  out_dir = "data/gfw_bulk",
+  out_dir = "data/gfw_bulk", #The data is saved in this directory
   quiet = FALSE,
   print_request = TRUE
 )
 
-res$paths$csv
-df_head <- read.csv(res$paths$csv, nrows = 5)
-print(df_head)
+#res$paths$csv
+df <- read.csv(res$paths$csv)#, nrows = 5)
+View(df)
